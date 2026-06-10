ee8ca69332723d890301e34459c86f4acd7432ce
Jonathan Kana <jokana2008@gmail.com>
2026-06-10 14:54:01 +0000

    Belajar Strings

--- fourth-day.py
+++ fourth-day.py
@@ -16,3 +16,30 @@ while i < 10:
 print("------------------------------")
 
 print("ya kurang lebih begitu yang gw pelajari hari ini, see you tomorrow!")
+print("I'm back malemnya, lanjut belacar ya ges ya")
+
+# contoh variabel deh ini
+
+jumlah_siswa = 150
+
+rating = 4.95
+
+is_published = True                    #mulai coba Boolean dikit
+
+nama_tugas = "Belajar Python"
+
+print(jumlah_siswa)
+
+print(len(nama_tugas))                 #ini buat ngitung panjang string, jadi ada 14 huruf di nama_tugas
+
+print(nama_tugas[0])                   #ini buat ngambil karakter pertama dari string
+
+print(nama_tugas[-1])                  #ini buat ngambil karakter terakhir dari string
+
+print(nama_tugas[0:7])                 #ini buat ngambil karakter dari indeks 0 sampai 6
+
+print(nama_tugas[0:])                  #ini buat ngambil semua karakter dari indeks 0 sampai akhir
+
+print(nama_tugas[:7])                  #ini buat ngambil karakter dari awal sampai indeks 6 karena default mulai dari 0
+
+print(nama_tugas[:])                   #ini buat ngambil semua karakter dari awal sampai akhir karena default mulai dari 0 sampai akhir
