5759c6c97235fd39168fc3cfa1f5dea4afd0a902
Jonathan Kana <jokana2008@gmail.com>
2026-06-10 09:27:11 +0000

    Belajar Looping menggunakan For dan While

---
+++ fourth-day.py
@@ -0,0 +1,18 @@
+print("yoo guys this me again!")
+print("welcome to day 4!")
+print("hari ini di aplikasi Briliant gw belajar tentang looping menggunakan for dan while")
+
+print("------------------------------")
+
+for i in range(6):
+    print("ini adalah perulangan ke-", i)
+
+print("------------------------------")
+
+while i < 10:
+    print("ini adalah perulangan ke-", i)
+    i += 1
+
+print("------------------------------")
+
+print("ya kurang lebih begitu yang gw pelajari hari ini, see you tomorrow!")
