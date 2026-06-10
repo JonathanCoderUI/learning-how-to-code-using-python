d70814d2be139d64d5d05a1a4bc55f0284a761d3
Jonathan Kana <jokana2008@gmail.com>
2026-06-10 14:59:42 +0000

    Dirapihin aja dikit

--- fourth-day.py
+++ fourth-day.py
@@ -26,22 +26,22 @@ jumlah_siswa = 150
 
 rating = 4.95
 
-is_published = True                    #mulai coba Boolean dikit
+is_published = True                    # mulai coba Boolean dikit
 
 nama_tugas = "Belajar Python"
 
 print(jumlah_siswa)
 
-print(len(nama_tugas))                 #ini buat ngitung panjang string, jadi ada 14 huruf di nama_tugas
+print(len(nama_tugas))                 # ini buat ngitung panjang string, jadi ada 14 huruf di nama_tugas
 
-print(nama_tugas[0])                   #ini buat ngambil karakter pertama dari string
+print(nama_tugas[0])                   # ini buat ngambil karakter pertama dari string
 
-print(nama_tugas[-1])                  #ini buat ngambil karakter terakhir dari string
+print(nama_tugas[-1])                  # ini buat ngambil karakter terakhir dari string
 
-print(nama_tugas[0:7])                 #ini buat ngambil karakter dari indeks 0 sampai 6
+print(nama_tugas[0:7])                 # ini buat ngambil karakter dari indeks 0 sampai 6
 
-print(nama_tugas[0:])                  #ini buat ngambil semua karakter dari indeks 0 sampai akhir
+print(nama_tugas[0:])                  # ini buat ngambil semua karakter dari indeks 0 sampai akhir
 
-print(nama_tugas[:7])                  #ini buat ngambil karakter dari awal sampai indeks 6 karena default mulai dari 0
+print(nama_tugas[:7])                  # ini buat ngambil karakter dari awal sampai indeks 6 karena default mulai dari 0
 
-print(nama_tugas[:])                   #ini buat ngambil semua karakter dari awal sampai akhir karena default mulai dari 0 sampai akhir
+print(nama_tugas[:])                   # ini buat ngambil semua karakter dari awal sampai akhir karena default mulai dari 0 sampai akhir
